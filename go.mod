bdda815ba8899caa545ab733949e968d3328f0c5
Packmeister <tcb@protonmail.com>
2026-01-06 18:55:09 -0800

    final commit?

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/PackMeister/depProject
 
 go 1.25.5
+
+require github.com/PackMeister/puppy v0.0.0-20260107011702-6dc309f405ac
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/PackMeister/puppy"
+)
+
 func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	// Or by
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
 
 }
